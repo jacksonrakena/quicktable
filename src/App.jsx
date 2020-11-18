e753821f2353830548d88c52bcba723ee1b8c43b
abyssal <jackson@rakena.co.nz>
2020-11-18 22:53:21 +1300

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -32,15 +32,58 @@ export default class App extends Component {
                 </nav>
                 <Switch>
                     <Route exact path="/app/:id" component={Schedule} />
-                    <Route exact path="/" component={Empty} />
+                    <Route exact path="/" component={Homepage} />
                 </Switch>
             </div>
         </BrowserRouter>
     }
 }
 
-class Empty extends Component {
+class Homepage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: ''
+        }
+
+        this.updateValue = this.updateValue.bind(this)
+    }
+
+    updateValue(v, d) {
+        this.setState({
+            id: v.target.value
+        })
+    }
+
     render() {
-        return <div>Hi.</div>
+        return <div className="content">
+            <div>
+                Welcome to the Quicktable beta.
+            </div>
+            <form>
+                <div class="form-row align-items-center justify-content-start">
+                    <div class="col-auto">
+                        <span>Please enter your student ID number:</span>
+                    </div>
+                    <div class="col-auto">
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="inlineFormInputGroup" onChange={this.updateValue}  placeholder="5 or 6 digits" />
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <NavLink to={`/app/${this.state.id}`}>
+                            <button type="submit" class="btn btn-primary mb-1">Enter</button>
+                        </NavLink>
+                    </div>
+                </div>
+            </form>
+            <footer class="footer">
+                <div class="container">
+                    <span class="text-muted">
+                        Copyright &copy; 2019-2021 Jackson Rakena
+                    </span>
+                </div>
+            </footer>
+        </div>
     }
 }
