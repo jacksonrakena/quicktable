d4be6a4557a3ec072d08f8a8f168901691e29353
abyssal <jackson@rakena.co.nz>
2021-03-03 09:17:48 +1300

    Added alert level status.

--- src/components/app/TodaySection.jsx
+++ src/components/app/TodaySection.jsx
@@ -4,7 +4,12 @@ export default class TodaySection extends Component {
     render() {
         return <div>
             <h4>Today</h4>
-            Have a good day. Make sure to check out the <a href="https://www.instagram.com/scots.co.nz/">@scots.co.nz</a> Instagram page.
+            <span style={{marginTop: '10px', marginBottom: '10px', color: 'black', background: 'repeating-linear-gradient(45deg, white, white 10px, yellow 10px, yellow 20px)', padding: '10px'}}>
+                <strong style={{backgroundColor: 'white', padding: '5px'}}>
+                Wellington is at Alert Level 2.
+                </strong>
+            </span><br /><br />
+            Make sure to check out the <a href="https://www.instagram.com/scots.co.nz/">@scots.co.nz</a> Instagram page.
         </div>
     }
 }
