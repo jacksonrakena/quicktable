6bca9dddf82c9e9663d8f7210c055cf4d99c1b8e
Jackson <jackson@rakena.co.nz>
2021-03-06 20:57:30 +1300

    Update TodaySection.jsx

--- src/components/app/TodaySection.jsx
+++ src/components/app/TodaySection.jsx
@@ -6,7 +6,7 @@ export default class TodaySection extends Component {
             <h4>Today</h4>
             <span style={{marginTop: '10px', marginBottom: '10px', color: 'black', background: 'repeating-linear-gradient(45deg, white, white 10px, yellow 10px, yellow 20px)', padding: '10px'}}>
                 <strong style={{backgroundColor: 'white', padding: '5px'}}>
-                Wellington is at Alert Level 2.
+                Wellington is at Alert Level 1.
                 </strong>
             </span><br /><br />
             Make sure to check out the <a href="https://www.instagram.com/scots.co.nz/">@scots.co.nz</a> Instagram page.
